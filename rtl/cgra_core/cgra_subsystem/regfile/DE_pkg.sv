9261919bc5f3d8ff14e80b3e9c3eeb25522195ad
Juwon Jun <jami3jun@linux-lab-134.ece.uw.edu>
2026-03-03 17:37:32 -0800

    Dispatcher size/logic reduction

--- rtl/cgra_core/cgra_subsystem/regfile/DE_pkg.sv
+++ rtl/cgra_core/cgra_subsystem/regfile/DE_pkg.sv
@@ -16,8 +16,8 @@ parameter int DICE_REG_ADDR_WIDTH = $clog2(DICE_NUM_REG_BANK)-1;
 // =========================================================
 // Dispatcher architecture constants
 // =========================================================
-parameter int NUM_SCOREBOARDS  = 4;
-parameter int NUM_LANES        = 4;
+parameter int NUM_SCOREBOARDS  = 1;
+parameter int NUM_LANES        = 1;
 parameter int CHUNK_SIZE       = `DICE_NUM_MAX_THREADS_PER_CORE / NUM_SCOREBOARDS;
 parameter int CHUNK_ADDR_WIDTH = (NUM_SCOREBOARDS == 1) ? 1 : $clog2(NUM_SCOREBOARDS);
 parameter int LANE_SIZE        = CHUNK_SIZE / NUM_LANES;
