5a935aa0cc45887bd52073a73cdf06b33d01b301
Elliot Norman <elliotjnorman@gmail.com>
2026-04-05 16:27:43 -0700

    added correct tb to filelist

--- tb/cgra_core/FDR_stage/branch_handler/filelist.f
+++ tb/cgra_core/FDR_stage/branch_handler/filelist.f
@@ -12,3 +12,6 @@ ${DICE_HOME}/rtl/dice_frontend_pkg.sv
 // ==== branch_handler dependencies ====
 ${DICE_HOME}/rtl/cgra_core/fetch_stage/rising_edge_detector.sv
 ${DICE_HOME}/rtl/cgra_core/fetch_stage/branch_handler.sv
+
+// ==== testbench ====
+${DICE_HOME}/tb/cgra_core/FDR_stage/branch_handler/tb_branch_handler.sv
